cc704968317823a35dd0c58bc7effa322459491a
Ira Levkovich <6346390@mail.ru>
2020-08-06 18:40:21 +0300

    Add tasks

---
+++ index.js
@@ -0,0 +1,44 @@
+// Task 1
+
+// let admin = null;
+// let name = null;
+// name = "Jhon";
+// admin = name;
+// alert( admin );
+
+
+// Task 2
+
+// const name = prompt("What is your name?", "");
+// alert(name);
+
+
+// Task 3
+
+// let value = prompt("Введите число", 0);
+// if (value > 0) {
+//     alert(1);
+// } else if (value < 0) {
+//     alert(-1);
+// } else {
+//     alert(0);
+// }
+
+
+// Task 4
+
+// let login = prompt("Кто там?", "");
+// if (login === "Админ") {
+//     let pass = prompt("Пароль?", "");
+//     if (pass === "Я главный") {
+//         alert("Здравствуйте!");
+//     } else if (pass === "" || pass === null) {
+//         alert("Отменено");
+//     } else {
+//         alert("Неверный пароль");
+//     }
+// } else if (login === "" || login === null) {
+//     alert("Отменено");
+// } else {
+//     alert("Я вас не знаю");
+// }
